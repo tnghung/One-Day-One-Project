2ffde05846aab1124255215c3dce28ad3698b1c3
tnghung <tnghung2812@gmail.com>
2022-05-08 20:48:13 +0700

    Finish day 2 project

--- Day2-Progress_Steps/script.js
+++ Day2-Progress_Steps/script.js
@@ -3,16 +3,50 @@ const next = document.getElementById('next');
 const prev = document.getElementById('prev');
 const progress = document.getElementById('progress');
 
+let currentActiveIndex = 1;
 next.addEventListener('click', function () {
-  progress.style.width = '35%';
-  circles[1].classList.add('active');
-  prev.attributes.removeNamedItem('disabled');
-  prev.classList.add('active');
+  currentActiveIndex++;
+  if (currentActiveIndex >= circles.length) {
+    currentActiveIndex = circles.length;
+    next.disabled = true;
+  }
+  update();
 });
 
 prev.addEventListener('click', function () {
-  progress.style.width = '0%';
-  circles[1].classList.remove('active');
-  prev.classList.remove('active');
-  prev.attributes.setNamedItem('disabled');
+  currentActiveIndex--;
+  if (currentActiveIndex <= 1) {
+    currentActiveIndex = 1;
+    prev.disabled = true;
+    prev.classList.remove('active');
+  }
+
+  update();
 });
+
+function update() {
+  circles.forEach((circle, i) => {
+    if (currentActiveIndex > i) {
+      circle.classList.add('active');
+      prev.classList.add('active');
+      prev.disabled = false;
+    } else {
+      circle.classList.remove('active');
+      next.classList.add('active');
+      next.disabled = false;
+    }
+    progress.style.width = ((currentActiveIndex - 1) / (circles.length - 1)) * 100 + '%';
+  });
+}
+
+const obj = {
+  name: 'hung',
+  age: 18,
+};
+
+for (const val of Object.keys(obj)) {
+  val = {
+    value: obj.val,
+  };
+  console.log();
+}
